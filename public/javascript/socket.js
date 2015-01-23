29946a01b2aac5f298410d956a4bf637f8b3ec60
Stephen Greco <sgreco94@gmail.com>
2015-01-23 00:25:34 -0500

    Added workaround for stupid facebook thing

--- public/javascript/socket.js
+++ public/javascript/socket.js
@@ -133,3 +133,22 @@ var upvote, downvote, force_update;
   });
 
 })();
+
+
+// Remove the ugly Facebook appended hash
+// <https://github.com/jaredhanson/passport-facebook/issues/12>
+(function removeFacebookAppendedHash() {
+  if (!window.location.hash || window.location.hash !== '#_=_')
+    return;
+  if (window.history && window.history.replaceState)
+    return window.history.replaceState("", document.title, window.location.pathname);
+  // Prevent scrolling by storing the page's current scroll offset
+  var scroll = {
+    top: document.body.scrollTop,
+    left: document.body.scrollLeft
+  };
+  window.location.hash = "";
+  // Restore the scroll offset, should be flicker free
+  document.body.scrollTop = scroll.top;
+  document.body.scrollLeft = scroll.left;
+}());
